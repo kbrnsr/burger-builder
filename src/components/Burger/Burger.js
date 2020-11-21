c36293190ae899275ed8627abf5b276d9e6d36ee
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-21 22:18:24 +0100

    Outputting dynamic BurgerIngredient componets  used kind of a funky syntax to generate the necessary arrays, but  the alternative would be to have two nested for loops (better?)

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,16 +1,25 @@
+import PropTypes from 'prop-types';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const Burger = () => {
+const Burger = (props) => {
+  const { ingredients } = props;
   const { BurgerView } = classes;
+  const transformedIngredients = Object.keys(ingredients)
+    .map((igKey) => [...Array(ingredients[igKey])]
+      // eslint-disable-next-line react/no-array-index-key
+      .map((_, i) => <BurgerIngredient key={igKey + i} type={igKey} />));
   return (
     <div className={BurgerView}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
+};
+Burger.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  ingredients: PropTypes.object.isRequired,
 };
 
 export default Burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,23 @@ import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: {
+        salad: 1,
+        bacon: 1,
+        cheese: 2,
+        meat: 2,
+      },
+    };
+  }
+
   render() {
+    const { ingredients } = this.state;
     return (
       <Auxiliary>
-        <Burger />
+        <Burger ingredients={ingredients} />
       </Auxiliary>
     );
   }
