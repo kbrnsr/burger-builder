572eabbbe9fbddfa854e366a5ab46b68885d4820
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-22 21:00:01 +0100

    Added proper favicon  Changed launch.json, don't know if it actually works  Removed junk files and config data from manifest.json  Reverted App.js to a functional compponent again

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-/* eslint-disable react/prefer-stateless-function */
-import { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Layout>
+      <BurgerBuilder />
+    </Layout>
+  </div>
+);
 
 export default App;
