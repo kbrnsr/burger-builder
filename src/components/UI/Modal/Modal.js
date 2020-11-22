b280a76df80a38137292b9da2470f0b8cb8edb02
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-22 19:07:50 +0100

    Changed OrderSummary and Modal component to class based components  for optimization, not neccessary, but good practice

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,26 +1,39 @@
 import PropTypes from 'prop-types';
+import { Component } from 'react';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 import Auxiliary from '../../../hoc/Auxiliary';
 
-const Modal = (props) => {
-  const { children, show, modalClosed } = props;
-  const { ModalCSS } = classes;
-  return (
-    <Auxiliary>
-      <Backdrop show={show} clicked={modalClosed} />
-      <div
-        className={ModalCSS}
-        style={{
-          transform: show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: show ? '1' : '0',
-        }}
-      >
-        {children}
-      </div>
-    </Auxiliary>
-  );
-};
+class Modal extends Component {
+  // This whole thing should be a functional component
+  shouldComponentUpdate(nextProps) {
+    const { show } = this.props;
+    return nextProps.show !== show;
+  }
+
+  componentDidUpdate() {
+  }
+
+  render() {
+    const { children, show, modalClosed } = this.props;
+    const { ModalCSS } = classes;
+    return (
+      <Auxiliary>
+        <Backdrop show={show} clicked={modalClosed} />
+        <div
+          className={ModalCSS}
+          style={{
+            transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: show ? '1' : '0',
+          }}
+        >
+          {children}
+        </div>
+      </Auxiliary>
+    );
+  }
+}
+
 Modal.propTypes = {
   // eslint-disable-next-line react/require-default-props
   children: PropTypes.node,
