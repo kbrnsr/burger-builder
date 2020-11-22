e657221be6a74c733134b503c623cda2f71841b6
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-22 17:00:42 +0100

    Well, SideDrawer component balloned quite well, problem was that  it had to connect to 'parent' component so it could show itself  or not based on prop values which defines which CSS classes it  would use, which defined its UI behavior Changed Layout from stateless to stateful component since it needs  to manage state for use as props in SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,22 +1,41 @@
+import { Component } from 'react';
 import PropTypes from 'prop-types';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => {
-  const { children } = props;
-  const { MainContent } = classes;
-  return (
-    <Auxiliary>
-      <Toolbar />
-      <SideDrawer />
-      <main className={MainContent}>
-        {children}
-      </main>
-    </Auxiliary>
-  );
-};
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSideDrawer: true,
+    };
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  }
+
+  render() {
+    const { showSideDrawer } = this.state;
+    const { children } = this.props;
+    const { MainContent } = classes;
+    return (
+      <Auxiliary>
+        <Toolbar />
+        <SideDrawer
+          open={showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={MainContent}>
+          {children}
+        </main>
+      </Auxiliary>
+    );
+  }
+}
+
 Layout.propTypes = { children: PropTypes.node };
 Layout.defaultProps = { children: null };
 
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,20 +1,37 @@
+import PropTypes from 'prop-types';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxiliary from '../../../hoc/Auxiliary';
 
-const SideDrawer = () => {
-  const { SideDrawerCSS, LogoCSS } = classes;
+const SideDrawer = (props) => {
+  const { closed, open } = props;
+  const {
+    SideDrawerCSS, LogoCSS, OpenCSS, CloseCSS,
+  } = classes;
+  let attachedClasses = [SideDrawerCSS, CloseCSS];
+  if (open) {
+    attachedClasses = [SideDrawerCSS, OpenCSS];
+  }
   return (
-    <div className={SideDrawerCSS}>
-      <div className={LogoCSS}>
-        <Logo />
-      </div>
+    <Auxiliary>
+      <Backdrop show={open} clicked={closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={LogoCSS}>
+          <Logo />
+        </div>
 
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Auxiliary>
   );
+};
+SideDrawer.propTypes = {
+  closed: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired,
 };
 
 export default SideDrawer;
