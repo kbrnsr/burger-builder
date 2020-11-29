931e3f1e6fb9bfd0958bfb59a21c9e690684ac41
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-29 23:50:20 +0100

    Changed styling for Order component Fetching orders from backend in Orders container

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,38 @@
+/* eslint-disable react/no-unused-state */
 import { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  }
+
+  componentDidMount() {
+    axios.get('/orders.json')
+      .then((res) => {
+        const fetchOrders = [];
+        Object.entries(res.data).map(([key, value]) => {
+          fetchOrders.push({
+            ...value,
+            id: key,
+          });
+          return key;
+        });
+        this.setState({
+          loading: false,
+          order: fetchOrders,
+        });
+        console.log(fetchOrders);
+      })
+      .catch((err) => {
+        this.setState({ loading: false });
+        console.error('{Orders} componentDidMount', err);
+      });
+  }
+
   render() {
     return (
       <div>
@@ -13,4 +43,4 @@ class Orders extends Component {
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
